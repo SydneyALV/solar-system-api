dfe545e508540605ebc6fabb89ffa845ad7abd00
Sydney Alberson <sydney.alberson@gmail.com>
2023-04-25 13:16:38 -0400

    Imported blueprint to init file

--- app/__init__.py
+++ app/__init__.py
@@ -4,4 +4,10 @@ from flask import Flask
 def create_app(test_config=None):
     app = Flask(__name__)
 
+    # import routes
+    from .routes import planet_bp
+    
+    # register the blueprint
+    app.register_blueprint(planet_bp)
+    
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 
 class Planet():
     def __init__(self, id, name, description, moon_count):
@@ -7,20 +7,28 @@ class Planet():
         self.description = description
         self.moon_count = moon_count
         
-planets= [
+planets = [
     Planet(1, "mercury", "partly moltenor liquid", 0),
     Planet(2, "Venus", "brightest planet", 0),
     Planet(3, "Earth", "habitable planet", 1),
 ]
 
-planets_bp = Blueprint("planets", __name__, url_prefix= "/planets")
-@planets_bp.route("", methods= ["GET"])
+planet_bp = Blueprint("planets", __name__, url_prefix="/planets")
+
+@planet_bp.route("", methods=["GET"])
 
 def handle_planets():
     planets_response = []
+    
     for planet in planets:
-        planets_response.append(planet)
-    return planets_response
+        planets_response.append({
+            "id": planet.id,
+            "name": planet.name,
+            "description": planet.description,
+            "moon count": planet.moon_count
+        })
+    
+    return jsonify(planets_response)
         
 
 
