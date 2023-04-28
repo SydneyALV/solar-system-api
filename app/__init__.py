0e2dd11baf80c570cd3e9c1a8922ad0c350f9b7c
Sydney Alberson <sydney.alberson@gmail.com>
2023-04-28 11:44:56 -0400

    Created Planet model and added to database

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,27 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app(test_config=None):
     app = Flask(__name__)
+
+    # set up the database
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/solar_system_development'
+    
+    # connect the db and migrate to our flask app
+    db.init_app(app)
+    migrate.init_app(app, db)
 
     # import routes
     from .routes import planet_bp
     
     # register the blueprint
     app.register_blueprint(planet_bp)
+
+    from app.models.planet import Planet
     
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,51 +1,51 @@
 from flask import Blueprint, jsonify, abort
+from app import db
+from app.models.planet import Planet
 
-class Planet():
-    def __init__(self, id, name, description, moon_count):
-        self.id = id
-        self.name = name
-        self.description = description
-        self.moon_count = moon_count
+# class Planet():
+#     def __init__(self, id, name, description, moon_count):
+#         self.id = id
+#         self.name = name
+#         self.description = description
+#         self.moon_count = moon_count
         
-planets = [
-    Planet(1, "mercury", "partly moltenor liquid", 0),
-    Planet(2, "Venus", "brightest planet", 0),
-    Planet(3, "Earth", "habitable planet", 1),
-]
+# planets = [
+#     Planet(1, "mercury", "partly moltenor liquid", 0),
+#     Planet(2, "Venus", "brightest planet", 0),
+#     Planet(3, "Earth", "habitable planet", 1),
+# ]
 
 planet_bp = Blueprint("planets", __name__, url_prefix="/planets")
 
-@planet_bp.route("", methods=["GET"])
+# @planet_bp.route("", methods=["GET"])
 
-def handle_planets():
-    planets_response = []
+# def handle_planets():
+#     planets_response = []
     
-    for planet in planets:
-        planets_response.append({
-            "id": planet.id,
-            "name": planet.name,
-            "description": planet.description,
-            "moon count": planet.moon_count
-        })
+#     for planet in planets:
+#         planets_response.append({
+#             "id": planet.id,
+#             "name": planet.name,
+#             "description": planet.description,
+#             "moon count": planet.moon_count
+#         })
     
-    return jsonify(planets_response)
-        
-@planet_bp.route("/<planet_id>", methods=["GET"])
-
-def get_one_planet(planet_id):
+#     return jsonify(planets_response)
     
-    try:
-        planet_id = int(planet_id)
-    except:
-        abort(400, f"Planet with id {planet_id} not valid id type")
+# @planet_bp.route("/<planet_id>", methods=["GET"])
 
-    for planet in planets:
-        if planet.id == planet_id:
-            return {
-            "id": planet.id,
-            "name": planet.name,
-            "description": planet.description,
-            "moon count": planet.moon_count
-        }
+# def get_one_planet(planet_id):
+    
+#     try:
+#         planet_id = int(planet_id)
+#     except:
+#         abort(400, f"Planet with id {planet_id} not valid id type")
 
-    abort(404, f"Planet with id {planet_id} not found")
+#     for planet in planets:
+#         if planet.id == planet_id:
+#             return {
+#             "id": planet.id,
+#             "name": planet.name,
+#             "description": planet.description,
+#             "moon count": planet.moon_count
+#         }
